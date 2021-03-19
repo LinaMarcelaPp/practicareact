03d0805a2abd7b306a100e6596ef60b266dfd216
LinaMarcelaPp <linapena402@gmail.com>
2021-03-19 02:10:01 -0500

    adicione segundo nombre completo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Lina Ma Peña</code>
+          <code>Lina Marcela Peña</code>
         </p>
         <a
           className="App-link"
